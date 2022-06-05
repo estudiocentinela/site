c34f64181203e70259a0ef31a59e169f10178c8e
estudiocentinela <106797732+estudiocentinela@users.noreply.github.com>
2022-06-05 00:32:26 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -30,3 +30,62 @@ listElements.forEach(listElement => {
 
     })
 });
+
+//Animaciones de la pagina (BarbaJs)
+
+function pageTransition() {
+
+    var tl = gsap.timeline();
+
+    tl.to('ul.transition li', { duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2 })
+    tl.to('ul.transition li', { duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})
+    tl.to(".Content", { duration: 0.5, opacity: 1, delay: -0.5 })
+
+}
+
+
+function  contentAnimation(){
+    var tl = gsap.timeline();
+
+    tl.from(".anim-nav", { duration: 0.9, translateY: -100, opacity: 0, stagger: 0.1, delay: 1 }),
+    tl.to("#txt-parallax-1", { duration: 0.9, translateY: -50, opacity: 1, delay: -0.6 }),
+    tl.to("#txt-parallax-2", { duration: 0.9, translateY: -60, opacity: 1, delay: -0.5 }),
+    tl.to("#txt-parallax-3", { duration: 0.9, translateY: -60, opacity: 1, delay: -0.4 });
+}
+
+
+function delay(n) {
+    n = n || 3000;
+    return new Promise(done => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+barba.init({
+
+    sync: true,
+
+    transitions: [{
+
+        async leave(data) {
+            const donde = this.async();
+
+            pageTransition();
+            await DelayNode(1500);
+            donde();
+        },
+
+        async enter(data) {
+            pageTransition();
+            contentAnimation();
+            window.scrollTo(0, 0);   
+        },
+
+        async once(data) {
+            pageTransition();
+            contentAnimation();
+        }
+    }]
+})
